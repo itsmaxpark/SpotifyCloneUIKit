f5b1ca3190320bf161b20b247fae9f82f8eef305
Max Park <itsmaxpark@gmail.com>
2022-03-04 19:13:06 -0500

    created view controllers for library tab created 2 sections with animated toggle button

---
+++ SpotifyCloneUIKit/Controllers/Other/Library/LibraryAlbumsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryAlbumsViewController.swift
+//  SpotifyCloneUIKit
+//
+//  Created by Max Park on 3/4/22.
+//
+
+import UIKit
+
+class LibraryAlbumsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemOrange
+    }
+}
---
+++ SpotifyCloneUIKit/Controllers/Other/Library/LibraryPlaylistsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryPlaylistsViewController.swift
+//  SpotifyCloneUIKit
+//
+//  Created by Max Park on 3/4/22.
+//
+
+import UIKit
+
+class LibraryPlaylistsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemPink
+    }
+}
